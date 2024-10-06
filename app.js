10db0b8b88fe43aa18c91363762166855e18c0a0
Joao Nolasco <joao.nolasco@tecnico.ulisboa.pt>
2024-10-06 22:29:46 +0100

    Updated homepage
    Added a preloader to the homepage. Changed the homapge's JS and CSS to dynamically add the links and change the navbar height based on a array of links that will be easier to update.

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
-const navToggle = document.querySelector('.nav-toggle');
+const linkList = [
+    {
+        id: 1,
+        title: "Counter",
+        path: "counter/index.html"
+    },
+    {
+        id: 2,
+        title: "Colour Flipper",
+        path: "flip/index.html"
+    },
+    {
+        id: 3,
+        title: "Review",
+        path: "review/index.html",
+    },
+    {
+        id: 4,
+        title: "Navbar",
+        path: "Navbar/index.html",
+    },
+    {
+        id: 5,
+        title: "Sidebar",
+        path: "sidebar/index.html",
+    }
+];
+
+/* Preloader */
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', function(){
+    preloader.classList.add('hide-preloader');
+})
+
+/* Links */
 const links = document.querySelector('.links');
 
+window.addEventListener('DOMContentLoaded', function(){
+    let displayLinks = linkList.map(function(item){
+        return (`<li>
+                    <a href=${item.path}>${item.title}</a>
+                 </li>`);
+    });
+    displayLinks = displayLinks.join('');
+    links.innerHTML = displayLinks;
+});
+
+
+/* Navbar */
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+
 navToggle.addEventListener('click', function(){
-    /*if (links.classList.contains('show-links')){
-        links.classList.remove('show-links');
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    console.log(linksHeight);
+
+    if (containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`;
     }
-    else{
-        links.classList.add('show-links');
-    }*/
-    links.classList.toggle('show-links');
+    else {
+        linksContainer.style.height = 0;
+    }
 });
