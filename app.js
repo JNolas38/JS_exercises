7a11645ffce2549c9539a310d35f709a6e5af57e
Joao Nolasco <joao.nolasco@tecnico.ulisboa.pt>
2024-10-23 01:13:01 +0100

    Added menu items for the exercises
    Added menu items to the homepage that discribe and link to the various completed exercises

--- app.js
+++ app.js
@@ -2,27 +2,32 @@ const linkList = [
     {
         id: 1,
         title: "Counter",
-        path: "counter/index.html"
+        path: "counter/index.html",
+        desc: "A counter that can be increased, decreased and reset with changing number colours."
     },
     {
         id: 2,
         title: "Colour Flipper",
-        path: "flip/index.html"
+        path: "flip/index.html",
+        desc: "A colour flipper that changes the pages background with either a set array of colour or a random hexadecimal value."
     },
     {
         id: 3,
         title: "Review",
         path: "review/index.html",
+        desc: "A page with multiple reviews to cycle through, either sequentially or at random"
     },
     {
         id: 4,
         title: "Navbar",
         path: "Navbar/index.html",
+        desc: "A page with a navbar that is hidden depending on the windows size"
     },
     {
         id: 5,
         title: "Sidebar",
         path: "sidebar/index.html",
+        desc: "A page with a sidebar that is hidden depending on the windows size"
     }
 ];
 
@@ -44,6 +49,8 @@ window.addEventListener('DOMContentLoaded', function(){
     });
     displayLinks = displayLinks.join('');
     links.innerHTML = displayLinks;
+
+    displayItems(linkList);
 });
 
 
@@ -65,7 +72,27 @@ navToggle.addEventListener('click', function(){
     }
 });
 
+/* Menu */
+const sectionCenter = document.querySelector('.section-center');
+function displayItems(menuItems){
+    let displayMenu = menuItems.map(function(item){
+        return(`<article class="menu-item">
+            <div class="item-info">
+                <header>
+                    <h4>${item.title}</h4>
+                    <h4 class="link-btn">
+                        <a href=${item.path}><i class="fas fa-eye"></i></a>
+                    </h4>
+                </header>
+                <p class="item-text">${item.desc}</p>
+            </div>
+        </article>`);
+    });
+    displayMenu = displayMenu.join('');
+    sectionCenter.innerHTML = displayMenu;
+};
+
+
 /* Date */
 const date = document.getElementById('date');
-
 date.innerHTML = new Date().getFullYear();
